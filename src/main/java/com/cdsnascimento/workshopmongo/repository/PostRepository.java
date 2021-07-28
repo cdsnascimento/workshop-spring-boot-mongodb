5dfb9e69094f48f2e01680db28f20fa16023c4ae
cdsnascimento <cdsnascimento@hotmail.com>
2021-07-27 23:30:36 -0300

    Consulta simples com @Query

--- src/main/java/com/cdsnascimento/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/cdsnascimento/workshopmongo/repository/PostRepository.java
@@ -5,10 +5,14 @@ import java.util.List;
 import com.cdsnascimento.workshopmongo.damain.Post;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+  @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+  List<Post> searchTitle(String text);
 
   List<Post> findByTitleContainingIgnoreCase(String text);
    
